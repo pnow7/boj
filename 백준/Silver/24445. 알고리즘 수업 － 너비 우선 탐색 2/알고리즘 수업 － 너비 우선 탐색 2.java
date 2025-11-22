2e34dfc90386da4066f7da5907bfd204a831005c
현재 <phyunjae7333@gmail.com>
2025-11-22 20:46:14 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 988 ms, Memory: 90840 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int[] order;
+	static int count = 1;
+	static ArrayList<Integer>[] graph;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int r = Integer.parseInt(st.nextToken());
+		
+		order = new int[n + 1];
+		graph = new ArrayList[n + 1];
+		
+		for (int i = 1; i <= n; i++) {
+			graph[i] = new ArrayList<>();
+		}
+		
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			
+			graph[u].add(v);
+			graph[v].add(u);
+		}
+		
+		for (int i = 1; i <= n; i++) {
+			Collections.sort(graph[i], Collections.reverseOrder());
+		}
+		
+		bfs(r);
+		
+		for (int i = 1; i <= n; i++) {
+			sb.append(order[i]).append("\n");
+		}
+		
+		System.out.println(sb.toString());
+		br.close();
+	}
+	
+	static void bfs(int startNode) {
+		Queue<Integer> q = new ArrayDeque<>();
+		
+		q.offer(startNode);
+		order[startNode] = count;
+		
+		while (!q.isEmpty()) {
+			int currentNode = q.poll();
+			
+			for (int nextNode : graph[currentNode]) {
+				if (order[nextNode] == 0) {
+					count++;
+					order[nextNode] = count;
+					q.offer(nextNode);
+				}
+			}
+		}
+	}
+	
+}
