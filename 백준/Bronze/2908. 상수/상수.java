a77c5b5640a4faf5757f4e7ac87c46afee298f02
현재 <phyunjae7333@gmail.com>
2025-11-29 21:45:56 +0900

    [Bronze II] Title: 상수, Time: 104 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,25 +1,20 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int A = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
-		int B = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
 		
-		if(A > B) {
-			System.out.println(A);
-		}
-		else {
-			System.out.println(B);
-		}
+		int constA = (a / 100) + (a % 10) * 100 + ((a - (a / 100) * 100) / 10) * 10;
+		int constB = (b / 100) + (b % 10) * 100 + ((b - (b / 100) * 100) / 10) * 10;
+		
+		System.out.println(Math.max(constA, constB));
 		br.close();
-		
 	}
-
+	
 }
