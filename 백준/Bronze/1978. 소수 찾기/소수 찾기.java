3ee557592809779919d5910fcb43ab29339a88c6
현재 <phyunjae7333@gmail.com>
2025-11-29 22:08:32 +0900

    [Bronze II] Title: 소수 찾기, Time: 100 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -1,39 +1,40 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
 
-	public static void main(String[] args) throws IOException{
+	private static int count = 0;
+	private static int check;
+	
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int count = 0;
-		
-		for(int i = 0; i < N; i++) {
+		for (int i = 0; i < n; i++) {
 			int num = Integer.parseInt(st.nextToken());
-			
-			if(num < 2) { //1 이하 소수 X
-				continue;
-			}
-			
-			boolean chk = true;
-			
-			for(int j = 2; j <= Math.sqrt(num); j++) {
-				if(num%j == 0) {
-					chk = false;
-					break;
-				}
-			}
-			
-			if(chk == true) {
-				count++;
-			}
-			
+			sol(num);
 		}
+		
 		System.out.println(count);
 		br.close();
 	}
-
+	
+	private static void sol(int n) {
+		if (n == 1) return;
+		
+		check = 0;
+		
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0 ) {
+				check++;
+			}
+		}
+		
+		if (check == 2) {
+			count++;
+		}
+		
+	}
+	
 }
