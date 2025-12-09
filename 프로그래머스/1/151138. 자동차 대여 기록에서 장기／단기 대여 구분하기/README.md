6c0e37d5ae8cd677f78c7c099846c2c6d54e2426
Hyeonjae <phyunjae7333@gmail.com>
2025-12-10 00:01:34 +0900

    [level 1] Title: 자동차 대여 기록에서 장기/단기 대여 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/151138. 자동차 대여 기록에서 장기／단기 대여 구분하기/README.md
+++ 프로그래머스/1/151138. 자동차 대여 기록에서 장기／단기 대여 구분하기/README.md
@@ -1,6 +1,6 @@
 # [level 1] 자동차 대여 기록에서 장기/단기 대여 구분하기 - 151138 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/151138) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/151138?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2025년 11월 27일 19:56:40
+2025년 12월 10일 00:01:32
 
 ### 문제 설명
 
--- 프로그래머스/1/151138. 자동차 대여 기록에서 장기／단기 대여 구분하기/자동차 대여 기록에서 장기／단기 대여 구분하기.sql
+++ 프로그래머스/1/151138. 자동차 대여 기록에서 장기／단기 대여 구분하기/자동차 대여 기록에서 장기／단기 대여 구분하기.sql
@@ -1,13 +1,13 @@
--- 코드를 입력하세요
+-- Oracle
 SELECT
-    HISTORY_ID,
-    CAR_ID,
-    DATE_FORMAT(START_DATE, '%Y-%m-%d') AS START_DATE,
-    DATE_FORMAT(END_DATE, '%Y-%m-%d') AS END_DATE,
+    history_id,
+    car_id,
+    TO_CHAR(start_date, 'YYYY-MM-DD') AS start_date,
+    TO_CHAR(end_date, 'YYYY-MM-DD') AS end_date,
     CASE 
-        WHEN DATEDIFF(END_DATE, START_DATE) >= 29 THEN '장기 대여'
+        WHEN end_date - start_date >= 29 THEN '장기 대여'
         ELSE '단기 대여'
-    END AS RENT_TYPE
-FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-WHERE START_DATE LIKE '2022-09%'
-ORDER BY HISTORY_ID DESC;
+    END AS rent_type
+FROM car_rental_company_rental_history
+WHERE TO_CHAR(start_date, 'YYYY-MM') = '2022-09'
+ORDER BY history_id DESC;
