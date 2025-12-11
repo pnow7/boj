0e8ab1ee0b87d2c86c359981427293dcdbcb7650
Hyeonjae <phyunjae7333@gmail.com>
2025-12-11 17:36:49 +0900

    [Bronze I] Title: 이 구역의 승자는 누구야?!, Time: 228 ms, Memory: 20016 KB -BaekjoonHub

--- 백준/Bronze/20154. 이 구역의 승자는 누구야？！/이 구역의 승자는 누구야？！.java
+++ 백준/Bronze/20154. 이 구역의 승자는 누구야？！/이 구역의 승자는 누구야？！.java
@@ -1,44 +1,26 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
 
-	private static final int[] ALPHABETVALUES = {
-			3, 2, 1, 2, 	3, 3	, 3, 3, 	1, 1,
+	private static int[] alphabetValues = {
+			3, 2, 1, 2, 	3, 3, 3, 3, 	1, 1,
 			3, 1, 3, 3, 	1, 2, 2, 2, 	1, 2,
 			1, 1, 2, 2, 2, 1
 	};
-	private static final String WINNER = "I'm a winner!";
-	private static final String LOSER = "You're the winner?";
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		List<Integer> list = new ArrayList<>();
 		
 		String word = br.readLine();
+		int n = word.length();
+		int totalSum = 0;
 		
-		for (char c : word.toCharArray()) {
-			list.add(ALPHABETVALUES[c - 'A']);
+		for (int i = 0; i < n; i++) {
+			int index = word.charAt(i) - 'A';
+			totalSum += alphabetValues[index];
 		}
 		
-		while (list.size() > 1) {
-			List<Integer> nextList = new ArrayList<>();
-			
-			for (int i = 0; i < list.size() / 2; i++) {
-				int sum = list.get(2 * i) + list.get(2 * i + 1);
-				nextList.add(sum % 10);
-			}
-			
-			if (list.size() % 2 != 0) {
-				nextList.add(list.get(list.size() - 1));
-			}
-			
-			list = nextList;
-		}
-		
-		int result = list.get(0);
-	
-		System.out.println(result % 2 != 0 ? WINNER : LOSER);
+		System.out.println(totalSum % 2 != 0 ? "I'm a winner!" : "You're the winner?");
 		br.close();
 	}
 
