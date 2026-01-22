76e2c3a9f53cd96bb9587a3ab66af1c49d1845bc
Hyeonjae <phyunjae7333@gmail.com>
2026-01-22 23:12:19 +0900

    [Bronze V] Title: X보다 작은 수, Time: 104 ms, Memory: 12016 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
@@ -1,25 +1,9 @@
 const fs = require('fs');
 const input = fs.readFileSync("/dev/stdin").toString().trim().split(/\s+/);
 
-let idx = 0;
-const N = Number(input[idx++]);
-const X = Number(input[idx++]);
-
-const arr = [];
-for (let i = 0; i < N; i++) {
-    arr.push(Number(input[idx++]));
-}
-
-const arrRtn = [];
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < X) {
-        arrRtn.push(arr[i]);
-    }
-}
-
-let result = "";
-for (let i = 0; i < arrRtn.length; i++) {
-    result += arrRtn[i] + " ";
-}
+const N = Number(input[0]);
+const X = Number(input[1]);
+const arr = input.slice(2).map(Number);
+const result = arr.filter(val => val < X).join(" ");
 
 console.log(result);
