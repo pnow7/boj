3a43e01c88d08924794c4941e6f2e27de683ac59
Hyeonjae <phyunjae7333@gmail.com>
2026-01-28 22:28:42 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 100 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
@@ -3,12 +3,11 @@ const input = fs.readFileSync("/dev/stdin").toString().trim();
 
 const result = Array(26).fill(-1);
 
-for (let i = 0; i < input.length; i++) {
-    const idx = input.charCodeAt(i) - 97;
-
+input.split('').forEach((ch, i) => {
+    const idx = ch.charCodeAt(0) - 97;
     if (result[idx] === -1) {
         result[idx] = i;
     }
-}
+})
 
 console.log(result.join(' '));
