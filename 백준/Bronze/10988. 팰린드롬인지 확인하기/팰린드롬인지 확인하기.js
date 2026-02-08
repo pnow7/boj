009d0b3de48537b9fe6febc786a8209182f33a21
Hyeonjae <phyunjae7333@gmail.com>
2026-02-08 22:09:23 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 88 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.js
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const input = fs.readFileSync("/dev/stdin").toString().trim().split("").join("");
+const input = fs.readFileSync("/dev/stdin").toString().trim();
 const input2 = [...input].reverse().join("");
 
 console.log(input === input2 ? 1 : 0);
