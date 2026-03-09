d258c7973684a4f42f6e20c18e52e66549e7abc9
Hyeonjae <phyunjae7333@gmail.com>
2026-03-09 14:55:29 +0900

    [Silver V] Title: 너의 평점은, Time: 92 ms, Memory: 9352 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const input = fs.readFileSync("/dev/stdin").toString().trim().split('\n');
+
+const GRADE = { 
+    "A+": 4.5, "A0": 4.0, 
+    "B+": 3.5, "B0": 3.0,
+    "C+": 2.5, "C0": 2.0, 
+    "D+": 1.5, "D0": 1.0, 
+    "F": 0.0
+};
+
+let totalScore = 0;   
+let totalCredits = 0;
+
+for (const line of input) {
+    const [_, credit, grade] = line.split(" ");
+    if (grade === "P") continue;
+
+    const point = GRADE[grade];
+    const creditNum = Number(credit);
+
+    totalScore += (point * creditNum);
+    totalCredits += creditNum;
+}
+
+console.log((totalScore / totalCredits).toFixed(6));
